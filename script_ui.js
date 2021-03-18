ac949f29c23d4b44bc745abcfb69c0cd922e6bd6
Yellow May <kwerep09@yahoo.com>
2021-03-18 18:24:22 +0100

    Completed

---
+++ script_ui.js
@@ -0,0 +1,25 @@
+"use strict";
+var navBtn = document.getElementById("navBtn");
+var nav = document.querySelector("nav");
+var htmlBody = document.querySelector("body");
+var navActive = false;
+if (navBtn && nav && htmlBody) {
+    var navBtnImg_1 = navBtn.querySelector("img");
+    if (navBtnImg_1) {
+        navBtn.addEventListener("click", function () {
+            if (navActive) {
+                nav.style.left = "-100%";
+                htmlBody.style.overflow = "auto";
+                navBtnImg_1.src = "/images/icon-hamburger.svg";
+                navActive = false;
+            }
+            else {
+                window.scrollTo(0, 0);
+                nav.style.left = "0";
+                htmlBody.style.overflow = "hidden";
+                navBtnImg_1.src = "/images/icon-close.svg";
+                navActive = true;
+            }
+        });
+    }
+}
---
+++ script_ui.ts
@@ -0,0 +1,25 @@
+const navBtn = document.getElementById("navBtn");
+const nav: HTMLElement | null = document.querySelector("nav");
+const htmlBody: HTMLElement | null = document.querySelector("body");
+let navActive: boolean = false;
+
+if (navBtn && nav && htmlBody) {
+	const navBtnImg: HTMLImageElement | null = navBtn.querySelector("img");
+
+	if (navBtnImg) {
+		navBtn.addEventListener("click", () => {
+			if (navActive) {
+				nav.style.left = "-100%";
+				htmlBody.style.overflow = "auto";
+				navBtnImg.src = "/images/icon-hamburger.svg";
+				navActive = false;
+			} else {
+				window.scrollTo(0, 0);
+				nav.style.left = "0";
+				htmlBody.style.overflow = "hidden";
+				navBtnImg.src = "/images/icon-close.svg";
+				navActive = true;
+			}
+		});
+	}
+}
